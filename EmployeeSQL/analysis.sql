d1712a898ab90d48809899ae8f6c11a79a2a4d97
Eric Donnelly <63479480+ericdonnelly@users.noreply.github.com>
2020-08-18 12:02:13 -0500

    Update analysis.sql

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -6,7 +6,7 @@ INNER JOIN Salaries On Employees.emp_no = Salaries.emp_no;
 /*List first name, last name, and hire date for employees who were hired in 1986.*/
 SELECT first_name, last_name, hire_date
 FROM Employees
-WHERE hire_date >= '1985-12-31'
+WHERE hire_date > '1985-12-31'
 AND hire_date < '1987-01-01';
 
 /*List the manager of each department with the following information: 
